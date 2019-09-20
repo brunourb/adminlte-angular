ffa47fdc3ee6a78b8cb3202ad353d01c47126329
Genuine-Identity <girish.nandgawe@gmail.com>
2019-09-21 02:14:39 +0530

    2 files modified

--- src/app/shared/components/mail-compose/mail-compose.module.ts
+++ src/app/shared/components/mail-compose/mail-compose.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { FormsModule } from '@angular/forms';
 import { MailComposeComponent } from './mail-compose.component';
 import { Routes, RouterModule } from '@angular/router';
 import { AceEditorModule } from 'ng2-ace-editor';
