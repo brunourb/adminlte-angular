2ad79be4f70a70a46787935ea2d58ec498eeafd6
Genuine-Identity <girish.nandgawe@gmail.com>
2019-09-20 22:49:03 +0530

    2 files modified

--- src/app/shared/components/mail-compose/mail-compose.component.ts
+++ src/app/shared/components/mail-compose/mail-compose.component.ts
@@ -1,10 +1,34 @@
 import { Component, Input, OnInit, Output, EventEmitter, ElementRef, Injectable, ViewChild, AfterViewInit } from '@angular/core';
+import { NgSelectModule, NgOption } from '@ng-select/ng-select';
+import { SkillService } from '../../../core/services/application/skill.service';
+
+
+import { first } from 'rxjs/operators'
+import { UserService } from '../../../core/services/application/user.service';
+import { User } from '../../../shared/models/user';
+import { AlertType, Alert } from '../../models/all';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { AlertModule } from '../../widgets/alert/alert.module';
+
+
+
 @Component({
   selector: 'app-mail-compose',
   templateUrl: './mail-compose.component.html',
   styleUrls: ['./mail-compose.component.css'],
 })
 export class MailComposeComponent implements OnInit {
+
+  id: number;
+  submitted = false;
+  alert: Alert;
+  user: User;
+
+  skills: NgOption[];
+  skillIds: any[]
+
+
   constructor() { }
   ngOnInit() { }
   @ViewChild('editor') editor;
--- src/app/shared/components/mail-compose/mail-compose.module.ts
+++ src/app/shared/components/mail-compose/mail-compose.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { MailComposeComponent } from './mail-compose.component';
 import { Routes, RouterModule } from '@angular/router';
 import { AceEditorModule } from 'ng2-ace-editor';
+
 @NgModule({
   imports: [
     CommonModule,
     RouterModule,
-    AceEditorModule
+    AceEditorModule,
+    NgSelectModule
   ],
   declarations: [
     MailComposeComponent,
