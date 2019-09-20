37902df5ae0f5edf59dd37b8f88afe56b2a27261
Genuine-Identity <girish.nandgawe@gmail.com>
2019-09-20 13:30:08 +0530

    2 files added

---
+++ src/app/modules/mail/mail-box/inbox/component/inbox.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mail-box-inbox',
+  templateUrl: './inbox.component.html',
+  styleUrls: ['./i.component.css'],
+})
+
+export class InboxComponent implements OnInit {
+  constructor() {
+  }
+  ngOnInit() {
+  }
+}
