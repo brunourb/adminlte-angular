d21511aaf918d38286d7d35ed314c34d649417e8
Genuine-Identity <girish.nandgawe@gmail.com>
2019-09-20 23:11:30 +0530

    1 file modified

--- src/app/shared/components/mail-compose/mail-compose.component.ts
+++ src/app/shared/components/mail-compose/mail-compose.component.ts
@@ -3,13 +3,13 @@ import { NgSelectModule, NgOption } from '@ng-select/ng-select';
 import { SkillService } from '../../../core/services/application/skill.service';
 
 
-import { first } from 'rxjs/operators'
-import { UserService } from '../../../core/services/application/user.service';
-import { User } from '../../../shared/models/user';
-import { AlertType, Alert } from '../../models/all';
-import { Observable } from 'rxjs';
-import { tap } from 'rxjs/operators';
-import { AlertModule } from '../../widgets/alert/alert.module';
+// import { first } from 'rxjs/operators'
+// import { UserService } from '../../../core/services/application/user.service';
+// import { User } from '../../../shared/models/user';
+// import { AlertType, Alert } from '../../models/all';
+// import { Observable } from 'rxjs';
+// import { tap } from 'rxjs/operators';
+// import { AlertModule } from '../../widgets/alert/alert.module';
 
 
 
@@ -19,23 +19,18 @@ import { AlertModule } from '../../widgets/alert/alert.module';
   styleUrls: ['./mail-compose.component.css'],
 })
 export class MailComposeComponent implements OnInit {
-
-  // id: number;
   submitted = false;
-  // alert: Alert;
-  // user: User;
-
   mailsTo: NgOption[];
-  // mailsTo: any[]
-
+  @ViewChild('editor') editor;
 
   constructor(
-    private skillService: SkillService
-  ) { }
+    private skillService: SkillService) {
+  }
+
   ngOnInit() {
     this.bindSkills();
   }
-  @ViewChild('editor') editor;
+
   ngAfterViewInit() {
     this.editor.setTheme("eclipse");
     this.editor.getEditor().setOptions({
@@ -47,9 +42,11 @@ export class MailComposeComponent implements OnInit {
       exec: function (editor) { }
     })
   }
+
   getValue() {
     console.log(this.editor.value);
   }
+
   bindSkills() {
     this.mailsTo = this.skillService.getAllSkill();
   }
