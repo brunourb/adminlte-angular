777899a4a628a36438e83e2e5b6f967de42ec9a8
Genuine-Identity <girish.nandgawe@gmail.com>
2019-09-20 13:39:50 +0530

    4 files added

---
+++ src/app/layout/mail/mail-layout.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'admin-layout',
+  templateUrl: './admin-layout.component.html',
+  styleUrls: ['./admin-layout.component.css'],
+})
+
+export class MailLayoutComponent implements OnInit {
+  constructor() { }
+  ngOnInit() {
+  }
+}
