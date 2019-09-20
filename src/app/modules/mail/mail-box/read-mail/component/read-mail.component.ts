86b1e3a1f0d88129249259cd320995b29f64e30d
Genuine-Identity <girish.nandgawe@gmail.com>
2019-09-20 16:20:23 +0530

    3 files added

---
+++ src/app/modules/mail/mail-box/read-mail/component/read-mail.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mail-box-read-mail',
+  templateUrl: './read-mail.component.html',
+  styleUrls: ['./read-mail.component.css'],
+})
+
+export class ReadMailComponent implements OnInit {
+  constructor() {
+  }
+  ngOnInit() {
+  }
+}
