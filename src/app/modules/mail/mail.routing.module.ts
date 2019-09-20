8628616eb4131517049686ee9afa10a792ca6b08
Genuine-Identity <girish.nandgawe@gmail.com>
2019-09-20 13:25:14 +0530

    1 file modified, 1 added, 1 deleted

--- src/app/modules/mail/mail.routing.module.ts
+++ src/app/modules/mail/mail.routing.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { DashboardComponent } from './dashboard.component';
-import { DashbardControlPanelComponent } from './control-panel/pages/dashboard-control-panel.component';
+import { MailComponent } from './mail.component';
+// import { DashbardControlPanelComponent } from './control-panel/pages/dashboard-control-panel.component';
 import { AuthGuardService as AuthGuard } from '../../core/services/guards/auth-guard.service';
 const routes: Routes = [
   {
     path: '',
-    component: DashboardComponent,
+    component: MailComponent,
     children: [
       {
-        path: '', component: DashbardControlPanelComponent,
+        // path: '', component: DashbardControlPanelComponent,
         canActivate: [AuthGuard],
         data: {
           breadcrumb: 'Dashboard',
