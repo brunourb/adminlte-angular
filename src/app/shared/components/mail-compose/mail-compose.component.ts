4d84a4b65b60a9378f1a0803282b5f0b913261a8
Genuine-Identity <girish.nandgawe@gmail.com>
2019-09-20 18:39:15 +0530

    2 files modified

--- src/app/shared/components/mail-compose/mail-compose.component.ts
+++ src/app/shared/components/mail-compose/mail-compose.component.ts
@@ -1,10 +1,33 @@
-import { Component, Input, OnInit, Output, EventEmitter, ElementRef, Injectable } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter, ElementRef, Injectable, ViewChild, AfterViewInit } from '@angular/core';
 @Component({
   selector: 'app-mail-compose',
   templateUrl: './mail-compose.component.html',
   styleUrls: ['./mail-compose.component.css'],
 })
 export class MailComposeComponent implements OnInit {
+   
   constructor() { }
   ngOnInit() { }
+  @ViewChild('editor') editor;
+  ngAfterViewInit() {
+    this.editor.getEditor().setOptions({
+      showLineNumbers: true,
+      tabSize: 2
+    });
+
+    this.editor.mode = 'text';
+     
+
+    this.editor.getEditor().commands.addCommand({
+      name: "showOtherCompletions",
+      bindKey: "Ctrl-.",
+      exec: function (editor) {
+      }
+    })
+  }
+
+  getValue() {
+    console.log(this.editor.value)
+    console.log(eval(this.editor.value));
+  }
 }
