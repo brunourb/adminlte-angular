46796cd0e43a4cf8c6a32baff63778eae5b676a3
Genuine-Identity <girish.nandgawe@gmail.com>
2019-09-20 18:13:25 +0530

    1 file modified

--- src/app/modules/mail/mail.routing.module.ts
+++ src/app/modules/mail/mail.routing.module.ts
@@ -40,7 +40,7 @@ const routes: Routes = [
             },
           },
           {
-            path: 'compose', component: ComposeMComponent,
+            path: 'compose', component: ComposeMailComponent,
             canActivate: [AuthGuard],
             data: {
               breadcrumb: 'Compose ',
