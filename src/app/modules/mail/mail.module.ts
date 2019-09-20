9a93850ea90f7cdac06575d6718306a496e166bc
Genuine-Identity <girish.nandgawe@gmail.com>
2019-09-20 17:32:58 +0530

    1 file modified

--- src/app/modules/mail/mail.module.ts
+++ src/app/modules/mail/mail.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { MailComponent } from './mail.component';
+
 import { MailRoutingModule } from './mail.routing.module';
 import { MailGridModule } from '../../shared/components/mail-grid/mail-grid.module';
 import { InboxComponent } from './mail-box/inbox/component/inbox.component';
 import { MailBoxRootComponent } from './mail-box/mail-box-root.component';
 import { ReadMailComponent } from './mail-box/read/component/read-mail.component';
+import { MailMenuComponent } from '../../shared/components/mail-menu/mail-menu.module';
+
 
 @NgModule({
   imports: [
