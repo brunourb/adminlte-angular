ab7eec6767ada8f4b783801214e534232cf26fc6
Genuine-Identity <girish.nandgawe@gmail.com>
2019-09-20 17:51:12 +0530

    2 files added

---
+++ src/app/modules/mail/mail-box/compose/component/compose.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mail-box-compose',
+  templateUrl: './compose.component.html',
+  styleUrls: ['./inbox.component.css'],
+})
+
+export class ComposeComponent implements OnInit {
+  constructor() {
+  }
+  ngOnInit() {
+  }
+}
