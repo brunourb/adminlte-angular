f73e73c7d6fd6a7f24466fb6cf92604407abe3c8
Genuine-Identity <girish.nandgawe@gmail.com>
2019-09-29 20:17:59 +0530

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,9 +24,7 @@ import { AppRoutingModule } from './app.routing.module';
 
 import { NgSelectModule } from '@ng-select/ng-select';
 import { AceEditorModule } from 'ng2-ace-editor';
-import { ChartsModule } from 'ng2-charts';
 
-import { ChartModule } from 'angular-highcharts';
 @NgModule({
   imports: [
     BrowserModule,
@@ -38,7 +36,7 @@ import { ChartModule } from 'angular-highcharts';
     CoreModule,
     NgSelectModule,
     AceEditorModule,
-    ChartsModule
+    // ChartsModule
   ],
   declarations: [
     AppComponent
--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -1,29 +1,29 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
 // import "jquery";
 // import * as $ from 'jquery';
 
-import { DashboardComponent } from './dashboard.component';
-import { CardDetailsModule } from '../../shared/components/card-details/card-details.module';
-import { DashbardControlPanelComponent } from './control-panel/pages/dashboard-control-panel.component'
-import { DashboardRoutingModule } from './dashboard.routing.module'
-import { DashbardVersionOneComponent } from './control-panel/dashboard-version-one/dashboard-version-one.component';
+import { DashboardComponent } from "./dashboard.component";
+import { CardDetailsModule } from "../../shared/components/card-details/card-details.module";
+import { DashbardControlPanelComponent } from "./control-panel/pages/dashboard-control-panel.component";
+import { DashboardRoutingModule } from "./dashboard.routing.module";
+import { DashbardVersionOneComponent } from "./control-panel/dashboard-version-one/dashboard-version-one.component";
 // import { ChartsModule } from 'ng2-charts';
-
+import { ChartsModule } from "ng2-charts";
+import { ChartModule } from "angular-highcharts";
 @NgModule({
   imports: [
     CommonModule,
     DashboardRoutingModule,
     FormsModule,
     CardDetailsModule,
-    // ChartsModule
+    ChartsModule
   ],
   declarations: [
     DashboardComponent,
     DashbardControlPanelComponent,
     DashbardVersionOneComponent
-  ],
+  ]
 })
-
-export class DashboardModule { }
+export class DashboardModule {}
