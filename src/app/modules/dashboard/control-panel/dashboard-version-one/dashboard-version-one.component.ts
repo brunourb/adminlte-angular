0d06cf729ba9feb4d0709e3a64b42d3d53c7d0f0
Genuine-Identity <girish.nandgawe@gmail.com>
2019-09-29 20:42:48 +0530

    3 files modified

--- src/app/modules/dashboard/control-panel/dashboard-version-one/dashboard-version-one.component.ts
+++ src/app/modules/dashboard/control-panel/dashboard-version-one/dashboard-version-one.component.ts
@@ -1,42 +1,49 @@
-import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
-import $ from 'jquery';
-
-import { AppSettings } from '../../../../core/services/application/app-settings.service'
-// import { ChartsModule } from 'ng2-charts';
+import { Component, Input, OnInit, Output, EventEmitter } from "@angular/core";
+import { AppSettings } from "../../../../core/services/application/app-settings.service";
+import { ChartOptions, ChartType, ChartDataSets } from "chart.js"; 
+import { Label } from "ng2-charts";
 
 @Component({
-  selector: 'dashboard-version-one',
-  templateUrl: './dashboard-version-one.component.html',
-  styleUrls: ['./dashboard-version-one.component.css'],
+  selector: "dashboard-version-one",
+  templateUrl: "./dashboard-version-one.component.html",
+  styleUrls: ["./dashboard-version-one.component.css"]
 })
-
 export class DashbardVersionOneComponent implements OnInit {
-  private cards: any;
+  public barChartLabels: Label[];
+  public barChartType: ChartType = "bar";
+  public barChartLegend = true;
+  public barChartPlugins = [];
 
-  constructor(private appSettings: AppSettings) {
-  }
+  public barChartOptions: ChartOptions = {
+    responsive: true
+  };
+
+  public barChartData: ChartDataSets[] = [
+    { data: [65, 59, 80, 81, 56, 55, 40], label: "Received Mails" },
+    { data: [28, 48, 40, 19, 86, 27, 90], label: "Sent Mails" }
+  ];
+
+  constructor(private appSettings: AppSettings) {}
 
   ngOnInit() {
-    this.bindCards(); 
+    this.bindBarChartLabels();
   }
 
-  clcikMe() {
-    this.bindCards();
+  bindBarChartLabels() {
+    this.barChartLabels = [
+      "2006",
+      "2007",
+      "2008",
+      "2009",
+      "2010",
+      "2011",
+      "2012",
+      "2013",
+      "2014",
+      "2015",
+      "2016",
+      "2017"
+    ];
   }
-
-  bindCards() {
-    this.appSettings.getCustomerDetails().subscribe(
-      cards =>
-        (
-          this.cards = cards
-        )
-    );
-  }
-
-  onValueChange($event) {
-    console.log('onValueChange start');
-    console.log($event);
-    alert($event);
-    console.log('onValueChange end');
-  }
+ 
 }
--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -3,15 +3,15 @@ import { CommonModule } from "@angular/common";
 import { FormsModule } from "@angular/forms";
 // import "jquery";
 // import * as $ from 'jquery';
-
 import { DashboardComponent } from "./dashboard.component";
 import { CardDetailsModule } from "../../shared/components/card-details/card-details.module";
 import { DashbardControlPanelComponent } from "./control-panel/pages/dashboard-control-panel.component";
 import { DashboardRoutingModule } from "./dashboard.routing.module";
 import { DashbardVersionOneComponent } from "./control-panel/dashboard-version-one/dashboard-version-one.component";
-// import { ChartsModule } from 'ng2-charts';
+ 
 import { ChartsModule } from "ng2-charts";
 import { ChartModule } from "angular-highcharts";
+
 @NgModule({
   imports: [
     CommonModule,
