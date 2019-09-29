d72700d9611f84405760266d6f40ccc604caccfc
Genuine-Identity <girish.nandgawe@gmail.com>
2019-09-29 19:21:35 +0530

    2 files added

---
+++ src/app/modules/dashboard/control-panel/dashboard-version-one/dashboard-version-one.component.ts
@@ -0,0 +1,40 @@
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import $ from 'jquery';
+
+import { AppSettings } from '../../../..//core/services/application/app-settings.service'
+@Component({
+  selector: 'dashboard-version-one',
+  templateUrl: './dashboard-version-one.component.html',
+  styleUrls: ['./dashboard-version-one.component.css'],
+})
+
+export class DashbardVersionOneComponent implements OnInit {
+  private cards: any;
+
+  constructor(private appSettings: AppSettings) {
+  }
+
+  ngOnInit() {
+    this.bindCards();
+  }
+
+  clcikMe() {
+    this.bindCards();
+  }
+
+  bindCards() {
+    this.appSettings.getCustomerDetails().subscribe(
+      cards =>
+        (
+          this.cards = cards
+        )
+    );
+  }
+
+  onValueChange($event) {
+    console.log('onValueChange start');
+    console.log($event);
+    alert($event);
+    console.log('onValueChange end');
+  }
+}
