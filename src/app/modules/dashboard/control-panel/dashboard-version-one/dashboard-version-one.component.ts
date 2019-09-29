4bdcc1fd3b1f09a8e21f7e19beac46a27b707348
Genuine-Identity <girish.nandgawe@gmail.com>
2019-09-29 23:17:14 +0530

    2 files modified

--- src/app/modules/dashboard/control-panel/dashboard-version-one/dashboard-version-one.component.ts
+++ src/app/modules/dashboard/control-panel/dashboard-version-one/dashboard-version-one.component.ts
@@ -2,6 +2,16 @@ import { Component, Input, OnInit, Output, EventEmitter } from "@angular/core";
 import { AppSettings } from "../../../../core/services/application/app-settings.service";
 import { ChartOptions, ChartType, ChartDataSets } from "chart.js";
 import { Label } from "ng2-charts";
+import { User } from "../../../../shared/models/user";
+import { Message } from "../../../../shared/models/message";
+import { LocalStorageService } from "../../../../core/services/helpers/local-storage.service";
+import { MessageService } from "../../../../core/services/application/message.service";
+import { _ } from "lodash";
+import {
+  PagedData,
+  CorporateEmployee,
+  Page
+} from "../../../../shared/models/page";
 
 @Component({
   selector: "dashboard-version-one",
@@ -9,6 +19,7 @@ import { Label } from "ng2-charts";
   styleUrls: ["./dashboard-version-one.component.css"]
 })
 export class DashbardVersionOneComponent implements OnInit {
+  private user: User;
   public barChartOptions: ChartOptions;
   public barChartPlugins = [];
   public barChartLabels: Label[];
@@ -16,11 +27,27 @@ export class DashbardVersionOneComponent implements OnInit {
 
   public barChartLegend = true;
   public barChartType: ChartType = "bar";
-  constructor() {}
+
+  page = new Page();
+  rows = new Array<Message>();
+  pageInfo: any;
+
+  constructor(
+    private messageService: MessageService,
+    private localStorage: LocalStorageService
+  ) {
+    this.page.pageNumber = 0;
+    this.page.size = 1000;
+  }
   ngOnInit() {
+    this.bindUserDetails();
     this.bindBarChartLabels();
     this.bindBarChartOptions();
     this.bindBarChartData();
+    this.getSendMailsDetails({ offset: 0 });
+  }
+  bindUserDetails() {
+    this.user = JSON.parse(this.localStorage.getItem("userSession"));
   }
   bindBarChartOptions() {
     this.barChartOptions = {
@@ -33,21 +60,47 @@ export class DashbardVersionOneComponent implements OnInit {
       { data: [28, 48, 40, 19, 86, 27, 90], label: "Sent Mails" }
     ];
   }
-
   bindBarChartLabels() {
     this.barChartLabels = [
-      "2006",
-      "2007",
-      "2008",
-      "2009",
-      "2010",
-      "2011",
-      "2012",
-      "2013",
-      "2014",
-      "2015",
-      "2016",
-      "2017"
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "11",
+      "12",
+      "13",
+      "14",
+      "15",
+      "16",
+      "17",
+      "18",
+      "19",
+      "20",
+      "21",
+      "22",
+      "23",
+      "24"
     ];
+  }
+  getSendMailsDetails(pageInfo) {
+    this.pageInfo = pageInfo;
+    this.page.pageNumber = pageInfo.offset;
+    this.messageService
+      .getFromMessages(this.page, this.user.username)
+      .subscribe(pagedData => {
+        this.page = pagedData.page;
+        this.rows = pagedData.data;
+        for (let i = 0; i < this.rows.length; i++) {
+          this.rows[i].hour = new Date(this.rows[i].time).getHours()+;
+        }
+        var x = _.countBy(this.rows, "hour");
+        console.log(x);
+      });
   }
 }
--- src/app/shared/models/message.ts
+++ src/app/shared/models/message.ts
@@ -2,22 +2,23 @@ type MessageType = "Junk" | "Starred" | "Trash";
 type Status = "Active" | "InActive";
 type InBoxComponentType = "Junk" | "Starred" | "Trash" | "Sent";
 export interface Message {
-  id: number,
-  from: string,
-  fromName: string,
-  to: string,
-  toName: string,
-  subject: string,
-  body: string,
-  fromTeam: string,
-  toTeam: string,
-  time: string,
-  type: MessageType,
-  toType: MessageType,
-  fromType: MessageType,
-  suggestion: string,
-  imgSource: string,
-  toStatus: Status,
-  fromStatus: Status,
-  InBoxComponentType: InBoxComponentType
+  id: number;
+  from: string;
+  fromName: string;
+  to: string;
+  toName: string;
+  subject: string;
+  body: string;
+  fromTeam: string;
+  toTeam: string;
+  time: string;
+  type: MessageType;
+  toType: MessageType;
+  fromType: MessageType;
+  suggestion: string;
+  imgSource: string;
+  toStatus: Status;
+  fromStatus: Status;
+  InBoxComponentType: InBoxComponentType;
+  hour: number;
 }
