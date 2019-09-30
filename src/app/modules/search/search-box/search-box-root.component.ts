bd12cb6b4127e6251b1c4fa677fa1a36cb2d2723
Genuine-Identity <girish.nandgawe@gmail.com>
2019-09-30 18:23:44 +0530

    5 files added

---
+++ src/app/modules/search/search-box/search-box-root.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mail-box-root',
+  template: '<router-outlet></router-outlet>',
+})
+export class SBoxRootComponent implements OnInit {
+  constructor() { }
+  ngOnInit() { }
+}
