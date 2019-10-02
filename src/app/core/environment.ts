281b9c6373f35f751de95f941b53da239a745c59
Genuine-Identity <girish.nandgawe@gmail.com>
2019-10-02 15:20:43 +0530

    1 file modified, 1 added

---
+++ src/app/core/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyC3yy_-Ag_uA_ROcfxIWoilsFkvEfYEQYE",
+    authDomain: "stackblitz-firebase-database.firebaseapp.com",
+    databaseURL: "https://stackblitz-firebase-database.firebaseio.com",
+    projectId: "stackblitz-firebase-database",
+    storageBucket: "stackblitz-firebase-database.appspot.com",
+    messagingSenderId: "786515768533",
+    appId: "1:786515768533:web:e4224f6ba3d90920f5de5c",
+    measurementId: "G-KGJ0F4E18W"
+  }
+};
