6afe46131b63fd1b9c388d96ab98aa64b7c47e71
Genuine-Identity <girish.nandgawe@gmail.com>
2019-10-03 16:36:51 +0530

    3 files modified

--- src/app/modules/system/system-log/component/system-log.component.ts
+++ src/app/modules/system/system-log/component/system-log.component.ts
@@ -2,19 +2,26 @@ import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute, Router } from "@angular/router";
 import { LoggerService } from "../../.../../../../core/services/application/logger.service";
 import { Observable } from "rxjs/Observable";
+import { PagedData, Page } from "../../../../shared/models/page";
 @Component({
   selector: "app-system-log",
   templateUrl: "./system-log.component.html",
   styleUrls: ["./system-log.component.css"]
 })
 export class SystemLogComponent implements OnInit {
-  public rows: Observable<any[]>;
-  constructor(private log: LoggerService) {}
+  public rows: Array<any[]>;
+  page = new Page();
+  loadingIndicator: boolean = false;
+  constructor(private log: LoggerService) {
+    this.page.pageNumber = 0;
+    this.page.size = 10;
+  }
   ngOnInit() {
     this.bindLog();
   }
   bindLog() {
+    this.loadingIndicator = true;
     this.rows = this.log.get();
-    console.log(this.rows);
+    this.loadingIndicator = false;
   }
 }
