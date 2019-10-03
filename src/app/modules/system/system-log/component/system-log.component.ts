e7dde85946210c3ef17d7d651c429f59ba7d3381
Genuine-Identity <girish.nandgawe@gmail.com>
2019-10-03 13:02:07 +0530

    2 files added

---
+++ src/app/modules/system/system-log/component/system-log.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
+ 
+@Component({
+  selector: "app-system-log",
+  templateUrl: "./system-log.component.html",
+  styleUrls: ["./system-log.component.css"],
+ 
+})
+export class SystemLogComponent implements OnInit {
+  constructor() {}
+  ngOnInit() {}
+  ngOnDestroy() {}
+}
