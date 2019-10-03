da19f0f06495764911498246d5702aca87aecc1a
Genuine-Identity <girish.nandgawe@gmail.com>
2019-10-03 13:03:42 +0530

    asdf

--- src/app/modules/system/system-log/component/system-log.component.ts
+++ src/app/modules/system/system-log/component/system-log.component.ts
@@ -1,14 +1,13 @@
 import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute, Router } from "@angular/router";
- 
+
 @Component({
   selector: "app-system-log",
-  templateUrl: "./system-log.component.html",
-  styleUrls: ["./system-log.component.css"],
- 
+  templateUrl: "./system-log.component.html", 
+  styleUrls: ["./system-log.component.css"]
 })
 export class SystemLogComponent implements OnInit {
-  constructor() {}
+  constructor() {} 
   ngOnInit() {}
   ngOnDestroy() {}
 }
