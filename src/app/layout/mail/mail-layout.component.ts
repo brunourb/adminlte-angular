0556bbe420abcefe775f9104a6a430b24db13f08
Genuine-Identity <girish.nandgawe@gmail.com>
2019-10-03 12:50:36 +0530

    4 files modified

--- src/app/layout/mail/mail-layout.component.ts
+++ src/app/layout/mail/mail-layout.component.ts
@@ -1,14 +1,14 @@
-import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+// import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+// import { ActivatedRoute, Router } from '@angular/router';
 
-@Component({
-  selector: 'admin-layout',
-  templateUrl: './admin-layout.component.html',
-  styleUrls: ['./admin-layout.component.css'],
-})
+// @Component({
+//   selector: 'admin-layout',
+//   templateUrl: './admin-layout.component.html',
+//   styleUrls: ['./admin-layout.component.css'],
+// })
 
-export class MailLayoutComponent implements OnInit {
-  constructor() { }
-  ngOnInit() {
-  }
-}
+// export class MailLayoutComponent implements OnInit {
+//   constructor() { }
+//   ngOnInit() {
+//   }
+// }
--- src/app/layout/mail/mail-layout.module.ts
+++ src/app/layout/mail/mail-layout.module.ts
@@ -1,29 +1,29 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { MailLayoutRoutingModule } from './mail-layout.routing.module'
-import { MailLayoutComponent } from './mail-layout.component';
-import { FooterModule } from '../../shared/components/footer/footer.module';
-import { HeaderModule } from '../../shared/components/header/header.module';
-import { MainSidebarModule } from '../../shared/components/main-sidebar/main-sidebar.module';
-import { ContentHeaderModule } from '../../shared/components/content-header/content-header.module'; 
-import { ControlSidebarModule } from '../../shared/components/control-sidebar/control-sidebar.module';
-import { ControlSidebarBackGroundModule } from '../../shared/components/control-sidebar-bg/control-sidebar-bg.module';
+// import { NgModule } from '@angular/core';
+// import { BrowserModule } from '@angular/platform-browser';
+// import { MailLayoutRoutingModule } from './mail-layout.routing.module'
+// import { MailLayoutComponent } from './mail-layout.component';
+// import { FooterModule } from '../../shared/components/footer/footer.module';
+// import { HeaderModule } from '../../shared/components/header/header.module';
+// import { MainSidebarModule } from '../../shared/components/main-sidebar/main-sidebar.module';
+// import { ContentHeaderModule } from '../../shared/components/content-header/content-header.module'; 
+// import { ControlSidebarModule } from '../../shared/components/control-sidebar/control-sidebar.module';
+// import { ControlSidebarBackGroundModule } from '../../shared/components/control-sidebar-bg/control-sidebar-bg.module';
 
-@NgModule({
-  imports: [
-    MailLayoutRoutingModule,
-    HeaderModule,
-    FooterModule,
-    MainSidebarModule,
-    ControlSidebarModule,
-    ControlSidebarBackGroundModule,
-    ContentHeaderModule,
-  ],
-  declarations: [
-    MailLayoutComponent
-  ],
-  exports: [
-    MailLayoutComponent
-  ]
-})
-export class AdminLayoutModule { }
+// @NgModule({
+//   imports: [
+//     MailLayoutRoutingModule,
+//     HeaderModule,
+//     FooterModule,
+//     MainSidebarModule,
+//     ControlSidebarModule,
+//     ControlSidebarBackGroundModule,
+//     ContentHeaderModule,
+//   ],
+//   declarations: [
+//     MailLayoutComponent
+//   ],
+//   exports: [
+//     MailLayoutComponent
+//   ]
+// })
+// export class AdminLayoutModule { }
